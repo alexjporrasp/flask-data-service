6fd76de9cf6c0e890b94df3f25f35d5cfff36ec6
Alex Porras <alexjporrasp@uma.es>
2020-10-20 12:35:30 +0200

    Basic search functionality.

--- main.py
+++ main.py
@@ -7,10 +7,38 @@ app.config['MONGO_URI'] = config.mongo_uri()
 data = model.model()
 data.init_app(app)
 
-@app.route('/meta/<string:story_id>', methods=['GET'])
-def stories_metadata(story_id):
-    return flask.jsonify(data.news_metadata(story_id))
+def _build_response(cursor, result, collection):
+    return {
+        'data': result,
+        'collection': collection,
+        'next': cursor
+    }
 
-@app.route('/news', methods=['GET'])
+@app.route('/stories/', methods=['GET'])
 def stories():
-    return flask.jsonify(data.news())
+    cursor, stories = data.stories(
+        int(flask.request.args.get('cursor', 0)),
+        int(flask.request.args.get('limit', 10))
+    )
+    return flask.jsonify(_build_response(cursor, stories, 'stories'))
+
+@app.route('/stories/meta/<string:story_id>', methods=['GET'])
+def stories_metadata(story_id):
+    cursor, stories = data.stories_metadata(
+        story_id,
+        int(flask.request.args.get('cursor', 0)),
+        int(flask.request.args.get('limit', 10))
+    )
+    return flask.jsonify(_build_response(cursor, stories, 'meta'))
+
+
+
+@app.route('/stories/search/', methods=['GET'])
+def search():
+    cursor, stories = data.search(
+        flask.request.args,
+        int(flask.request.args.get('cursor', 0)),
+        int(flask.request.args.get('limit', 10))
+    )
+    
+    return flask.jsonify(_build_response(cursor, stories, 'stories'))
--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import flask_pymongo as pymongo
+import re
 from . import config
 
 def model():
@@ -16,20 +17,84 @@ class MongoDB:
         cls.mongo.init_app(app)
     
     @classmethod
-    def news_metadata(cls, story_id, cursor=0, limit=10):
-        return list(
+    def stories_metadata(cls, story_id, cursor=0, limit=10):
+        meta = list(
             cls.mongo.db.Structured.find(
                 {'rp_story_id': story_id},
                 {'_id': 0}
             ).skip(cursor).limit(limit)
         )
 
+        next_cursor = cursor + limit if len(meta) == limit else None
+
+        return next_cursor, meta
+
     @classmethod
-    def news(cls, cursor=0, limit=10):
-        return list(
+    def stories(cls, cursor=0, limit=10):
+        stories = list(
             cls.mongo.db.Unstructured.find(
                 {},
                 {'_id': 0}
             ).skip(cursor).limit(limit)
         )
+
+        next_cursor = cursor + limit if len(stories) == limit else None
+
+        return next_cursor, stories
+
+    @classmethod
+    def _numeric_modifier(cls, param: str):
+        match = re.match(r'(<|>|(<=)|(>=)|(==))((0|[1-9][0-9]*)(\.[0-9]*)?)', param)
+        if match:
+            op = match.group(1)
+            val = float(match.group(5))
+            if op == '>':
+                return {'$gt': val}
+            if op == '<':
+                return {'$lt': val}
+            if op == '>=':
+                return {'$gte': val}
+            if op == '<=':
+                return {'$lte': val}
+            return val
+        return None
     
+    @classmethod
+    def search(cls, params=None, cursor=0, limit=10):
+        if not params:
+            return cls.stories()
+
+        query = {}
+        entity_name = params.get('entity_name', None)
+        if entity_name:
+            query['entity_name'] = entity_name
+        score1 = params.get('Score1', '')
+        modifier = cls._numeric_modifier(score1)
+        if score1 and modifier:
+            query['Score1'] = modifier
+        score2 = params.get('Score2', '')
+        modifier = cls._numeric_modifier(score2)
+        if params.get('Score2', None):
+            query['Score2'] = modifier
+        if params.get('timestamp_utc', None):
+            pass
+
+        ids = list(
+            cls.mongo.db.Structured.find(
+                query,
+                {'rp_story_id': 1, '_id': 0}
+            ).sort([('relevance', -1)]).skip(cursor).limit(limit)
+        )
+
+        next_cursor = cursor + limit if len(ids) == limit else None
+
+        stories = []
+        for id in set(map(lambda x: x['rp_story_id'], ids)):
+            stories.extend(list(
+                cls.mongo.db.Unstructured.find(
+                    {'rp_story_id': id},
+                    {'_id': 0}
+                )
+            ))
+
+        return next_cursor, stories
